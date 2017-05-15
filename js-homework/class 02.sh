d187b83109d339559d82c4ea27cf2b43e7f9dac3
Kash Rajagopal <kash.rg@gmail.com>
2017-05-14 21:35:32 -0400

    updated hw class 2

---
+++ js-homework/class 02.sh
@@ -0,0 +1,36 @@
+# // assignment 2
+# //in feedback, would love to learn how this is done in industry. 
+# //Typically on the job, do you save this file down as a .bash?
+# // or a .js/.sh and call it in terminal somehow? 
+
+!/bin/bash    
+
+# for the above, stackoverflow said to add "#!/bin/bash" but it looked commented out when I did that
+
+
+cd ~/
+pwd
+mkdir -p js58/Sites/my-awesome-directory/nested-directory
+cd js58
+cd Sites
+cd my-awesome-directory
+cd nested-directory
+touch hello.txt
+cd ..
+cd ..
+touch terminals-are-fun.txt
+
+cd ~/
+cd js58
+cd Sites
+mkdir -p 1/2/3/4/5/6/7/8/9/hello/its/me
+cd 1/2/3/4/5/6/7/8/9/hello/its/me
+touch adele.jpg
+
+cd ~/
+cd js58/Sites
+mkdir -p i/was/wondering/'if'/after/all/these/years/you\ would/like/to/meet
+touch meet.jpg
+
+
+chmod +x ~/Kash/js58/js-homework
